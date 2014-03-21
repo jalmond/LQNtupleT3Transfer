b3a5c702d2fd670fa7c75cde4c226187597682a0
jalmond <jalmond@cern.ch>
2014-03-22 01:53:54 +0900

    add line to remove dir

--- dotransfer.py
+++ dotransfer.py
@@ -3,11 +3,11 @@ import os
 
 os.system("voms-proxy-init  cms")
 
-
 user = "jalmond"
-samples  = ["HNEE100" , "HNEE125", "HNEE150", "HNEE175", "HNEE200", "HNEE225", "HNEE250", "HNEE275," "HNEE300", "HNEE325", "HNEE350", "HNEE375", "HNEE400", "HNEE50", "HNEE500", "HNEE600", "HNEE70", "HNEE700", "HNEE90" ]
+samples  = ["HNEE100"]
 
 version = "LQ_Mar14"
+
 path = "/data1/DATA/LQNtuples_5_3_14/MC/"
 #path = "/data1/DATA/LQNtuples_5_3_14/Data/"
 
@@ -15,5 +15,5 @@ for s in samples:
     sample = version + s 
     endpath = path + s
     makeTransferFile_snu("cms2",sample, endpath, user )    
-
+    os.system("rm -r "+ sample)
     
