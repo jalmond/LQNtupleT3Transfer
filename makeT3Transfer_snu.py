fcb9106ad2a5baea564cbfac459bd2312c5945ba
jalmond <jalmond@cern.ch>
2014-03-22 01:46:18 +0900

    update code to remove duplicatesQ

--- makeT3Transfer_snu.py
+++ makeT3Transfer_snu.py
@@ -8,7 +8,7 @@ def makeTransferFile_snu(snumachine, sample, endpath, username):
 	toremove = path+dir
 
         ###### DO COPY
-
+	print "Setting up transfer"
 	if not (os.path.exists(endpath)):
 		os.system("mkdir " + endpath)
 	if not (os.path.exists(endpath)):
@@ -85,8 +85,100 @@ def makeTransferFile_snu(snumachine, sample, endpath, username):
 
 	os.system("grep -Fxv -f  " + sample + "/files_at_snuend_v2.txt " + sample + "/fullsamplelist.txt")
 
-#	os.system("rm " + sample + "/*.txt")	
+	
+	if not "MC" in endpath:
+		dir = endpath
+		tocheck = "rootTupleMaker_CRAB_DATA_2012_53X_"
+		print "Number of files in directory = "
+		count = os.system("ls -1 " + str(dir) + "  | wc -l")
+		os.system("cd " + str(dir) );
+		os.system("ls " + str(dir) + "/*.root > "+ sample+ "/duplcheck.txt")
+		isfile = os.path.isfile
+		join = os.path.join
+		number_of_files = sum(1 for item in os.listdir(dir) if isfile(join(dir, item)))
 
+		print "Checking for duplicated Job ID :" + tocheck + "[jobid]" + "_##.root"
+
+		count = number_of_files
+		dupl =0
+		for i in range(1, count):
+			fullname = tocheck + str(i) + "_"
+			nrepeat=0
+			fr = open(sample + '/duplcheck.txt','r')
+			for line in fr:
+				if fullname in line:
+					nrepeat+=1
+					print "Job " + str(i)  + ": --> " +  line
+					if nrepeat != 1:
+						print "FOUND TWICE: removing " + line
+						os.system("rm " + line)
+						dupl+=1
+
+						### remove duplicate on tier3
+						#os.system("xrd " + machine + " ls " + path + sample + "/" + " > " + sample + "/dupl" + sample +".txt")
+						#os.system("sed -r 's/^.{43}//' " + sample + "/dupl" + sample + ".txt > " + sample + "/duplclean.txt")
+						#os.system("cut -d/ -f 8 " + sample + "/duplclean.txt > " + sample + "/duplfullsample.txt")
+						ft3 = open(sample+'/fullsamplelist.txt' ,'r')
+						for t3line in ft3:
+							# copy to local dir
+							if "rootTupleMaker_CRAB_DATA" in t3line:
+								if line in t3line:
+									clean = "xrd " + machine + " rm "
+									fullpatht3 = path + sample + "/" + t3line
+									os.system(clean+fullpatht3)
+									print "Removing file " + fullpatht3 + " from tier3" 
+						
+	        if dupl == 0:
+			print "No duplicated jobs in directory"
+
+		print "Number of files in directory after check = "
+		count = os.system("ls -1 " + str(dir) + "  | wc -l")
+		
+	else:
+		dir = endpath
+		tocheck = "file_"
+		print "Number of files in directory = "
+		count = os.system("ls -1 " + str(dir) + "  | wc -l")
+		os.system("cd " + str(dir) );
+		os.system("ls " + str(dir) + "/*.root > " + sample+"/duplcheck.txt")
+		isfile = os.path.isfile
+		join = os.path.join
+		number_of_files = sum(1 for item in os.listdir(dir) if isfile(join(dir, item)))
+		
+		print "Checking for duplicated Job ID :" + tocheck + "[jobid]" + "_##.root"
+		
+		count = number_of_files
+		dupl =0
+		for i in range(1, count):
+			fullname = tocheck + str(i) + "_"
+			nrepeat=0
+			fr = open(sample + '/duplcheck.txt','r')
+			for line in fr:
+				if fullname in line:
+					nrepeat+=1
+					print "Job " + str(i)  + ": --> " +  line
+					if nrepeat != 1:
+						print "FOUND TWICE: removing " + line
+						os.system("rm " + line)
+						dupl+=1
+						### remove duplicate on tier3
+						#os.system("xrd " + machine + " ls " + path + sample + "/" + " > " + sample + "/dupl" + sample +".txt")
+						#os.system("sed -r 's/^.{43}//' " + sample + "/dupl" + sample + ".txt > " + sample + "/duplclean.txt")
+						#os.system("cut -d/ -f 8 " + sample + "/duplclean.txt > " + sample + "/duplfullsample.txt")
+						ft3 = open(sample+'/fullsamplelist.txt' ,'r')
+						for t3line in ft3:
+							if "file" in t3line:
+								# copy to local dir
+								if t3line in line:
+									clean = "xrd " + machine + " rm "
+									fullpatht3 = path + sample + "/" + t3line
+									os.system(clean+fullpatht3)
+									print "Removing file " + fullpatht3 + " from tier3"
+      	     	if dupl == 0:
+			print "No duplicated jobs in directory"
+		print "Number of files in directory after check = "
+		count = os.system("ls -1 " + str(dir) + "  | wc -l")
+		
 def rename(snumachine, sample, endpath):
 	import os
 	rename=True
