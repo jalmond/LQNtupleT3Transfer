e80127a23308c839653eaa6daedaf2c9b53e57a4
jalmond <jalmond@cern.ch>
2014-04-25 12:45:35 +0200

    Add signal transfer file

---
+++ dotransferSignal.py
@@ -0,0 +1,24 @@
+from makeT3Transfer_snu import *
+import os
+
+os.system("voms-proxy-init  cms")
+
+######  DO NOT CHANGE
+
+path = "/data1/DATA/LQNtuples_5_3_14_snu27/MC/"
+
+#####  USER MUST CHANGE
+user = "jalmond"
+
+sigsamples  = ["60"]
+#    "50","70","80","90","100","125","150","175","200","225","250","275","300","325","350","375","400","500","600","700"]
+#["MajoranaNeutrinoToEE_M-100_TuneZ2star_8TeV-alpgen"]
+
+for s in sigsamples:
+    channel = ["EE","MuMu"]
+    for c in channel:
+        sigs = "MajoranaNeutrinoTo"+ c + "_M-"+ s + "_TuneZ2star_8TeV-alpgen"
+        endpath = path + sigs
+        makeTransferFile_snu("cms2",sigs, endpath, user )
+        os.system("rm -r "+ sigs)
+        
